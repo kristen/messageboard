61e47c1d6e836c5731aa001164ecaa033f8810a4
Kristen Sundquist <kristen@csc8.com>
2013-07-27 14:46:22 -0700

    Add create for post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130727183304) do
+ActiveRecord::Schema.define(:version => 20130727214118) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.datetime "created_at",                             :null => false
