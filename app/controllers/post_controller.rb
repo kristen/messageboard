144b59648265c1ec8b3167d45cd24e2bde3c8a99
Kristen Sundquist <kristen@csc8.com>
2013-07-27 14:56:26 -0700

    Add create to Post

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -4,18 +4,18 @@ class PostController < ApplicationController
   end
 
   def create
-	@post = Post.new(post_params)
-
-	@post.save
-	redirect_to @post
+    @post = Post.new(params[:post])
+ 
+    @post.save
+    redirect_to action: :show, id: @post.id
   end
 
   def show
-  	@post = Post.find(param[:id])
+  	@post = Post.find(params[:id])
   end
 
-  private
-  	def post_params
-  		params.require(:post).permit(:title, :text)
-  	end
+  # private
+  # 	def post_params
+  # 		params.require(:post).permit(:title, :text)
+  # 	end
 end
